d14c8e9c57053625bc561b11f954ffae434f967e
Stefi <144570099+stefi1211@users.noreply.github.com>
2023-10-18 22:07:34 +0000

    my script

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// Listas de pronombres, adjetivos, sustantivos y extensiones
+let pronouns = ['the', 'our', 'my'];
+let adjectives = ['great', 'big', 'cool'];
+let nouns = ['jogger', 'racoon', 'website'];
+let extensions = ['.com', '.net', '.us', '.io', '.es'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateDomainCombinations() {
+  let domainCombinations = [];
+
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          let domain = pronouns[i] + adjectives[j] + nouns[k] + extensions[l];
+          domainCombinations.push(domain);
+        }
+      }
+    }
+  }
+
+  return domainCombinations;
+}
+
+
+let allDomainCombinations = generateDomainCombinations();
+
+
+allDomainCombinations.forEach(domain => {
+  console.log(domain);
+});
